60e481578aa0424051c0f4d834a8389c04c343a2
MahimaGoyal2023 <134344237+MahimaGoyal2023@users.noreply.github.com>
2023-05-25 17:23:27 +0530

    Update streamlit_app.py
    removed stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,9 +57,8 @@ if streamlit.button('Get Fruit load list'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
-streamlit.stop()
-
 streamlit.header("the fruit load list contains:")
+
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
     my_cur.execute("insert into fruit_load_list values('from streamlit')")
